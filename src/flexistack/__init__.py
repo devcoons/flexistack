a24f33eb84458edd9d3ecf4c7aca251f88bb5caf
devcoons <i_-_-_s@outlook.com>
2024-03-18 20:50:26 +0200

    update

--- src/flexistack/__init__.py
+++ src/flexistack/__init__.py
@@ -1,6 +1,6 @@
 from .flexistack import Flexistack, safe_import
 from .helper import Helper
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __name__ = "flexistack"
 __all__ = ['Flexistack',"Helper",'safe_import']
